8a66737bdde52f4822495547dc81244de57fdf95
Júnior Silva <junior.silva@alu.ufc.br>
2025-01-30 15:05:13 -0300

    notification implemented

---
+++ app/src/main/java/br/ufc/quixada/investidorapp/ui/view/InvertidorScreen.kt
@@ -0,0 +1,26 @@
+package br.ufc.quixada.investidorapp.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun InvestidorScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun ListaInvestimentos(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun InvestimentoItem(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun InvestidorScreenPreview() {
+    InvestidorScreen()
+}
